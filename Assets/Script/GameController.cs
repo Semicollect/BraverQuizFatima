3b8e752afbb50544f01a701be53dc920d17624f1
Maplewing <sinmaplewing@gmail.com>
2014-05-17 05:44:52 +0800

    入場態完工。

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+    public enum State { None, BattleStart };
+    private State _state = State.BattleStart;
+
+    public GameObject player;
+
+
 	// Use this for initialization
 	void Start () {
-        print(Character.PlayerType.ToString());
+		//player.GetComponent<SpriteRenderer>().sprite = knightSprite;
+		//player.GetComponent<Animator>().runtimeAnimatorController = knightAnimatorController;
+		//player.transform.localEulerAngles = new Vector3 (0, 180, 0);
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+        BattleStart();
 	}
+
+    void BattleStart()
+    {
+        if (_state == State.BattleStart)
+        {
+			if( player.transform.localPosition.x < -2 ){
+				player.transform.Translate(0.1f, 0, 0);
+			}
+        }
+    }
 }
