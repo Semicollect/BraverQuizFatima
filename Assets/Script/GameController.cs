df6d65e1c3a68271583e67640504fde2460e43bd
Maplewing <sinmaplewing@gmail.com>
2014-05-17 06:47:22 +0800

    Question Appeared.

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-    public enum State { None, BattleStart, Running, MonsterAppear };
+    public enum State { None, BattleStart, Running, MonsterAppear, DisplayQuestion };
     private State _state = State.BattleStart;
 
     public GameObject player;
 	public GameObject monster;
+	public GameObject planeGround;
+
+	public GameObject Q, A, B, C, D;
 
 	private GameObject _nowMonster = null;
 	private int _timer = 0;
+	private bool _questionSelected = false;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +26,7 @@ public class GameController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		DisplayQuestion ();
 		MonsterAppear ();
 		Running ();
         BattleStart();
@@ -53,10 +58,47 @@ public class GameController : MonoBehaviour {
 
 	void MonsterAppear(){
 		if( _state == State.MonsterAppear ){
-			if( _nowMonster == null ) (_nowMonster = (Object.Instantiate(monster) as GameObject)).transform.position = new Vector3(4.45f, 1.40f, 17f);
-			if(_nowMonster.transform.position.x > 1.1f){
+			if( _nowMonster == null ){
+				(_nowMonster = (Object.Instantiate(monster) as GameObject)).transform.position = new Vector3(4.45f, 1.40f, 17f);
+				planeGround.GetComponent<Animator>().enabled = false;
+			}
+			else if(_nowMonster.transform.position.x > 1.1f){
 				_nowMonster.transform.position -= new Vector3(0.1f, 0, 0);
 			}
+			else {
+				_state = State.DisplayQuestion;
+				_questionSelected = false;
+			}
 		}
+	}
+
+	void DisplayQuestion(){
+		if (_state == State.DisplayQuestion) {
+			if( !_questionSelected ){
+				Q.GetComponentInChildren<GUIText>().text = "今年是幾年？";
+				A.GetComponentInChildren<GUIText>().text = "2012";
+				B.GetComponentInChildren<GUIText>().text = "2013";
+				C.GetComponentInChildren<GUIText>().text = "2014";
+				D.GetComponentInChildren<GUIText>().text = "2015";
+				_questionSelected = true;
+			}
+
+			if( Q.guiTexture.color.a < 1 ){
+				Q.guiTexture.color += new Color( 0, 0, 0, 0.05f );
+			}
+			else if( A.transform.position.x < 0.5 ){
+				A.transform.Translate(0.1f, 0, 0, 0);
+			}
+			else if( B.transform.position.x > 0.5 ){
+				B.transform.Translate(-0.1f, 0, 0, 0);
+			}
+			else if( C.transform.position.x < 0.5 ){
+				C.transform.Translate(0.1f, 0, 0, 0);
+			}
+			else if( D.transform.position.x > 0.5 ){
+				D.transform.Translate(-0.1f, 0, 0, 0);
+			}
+		}
+
 	}
 }
