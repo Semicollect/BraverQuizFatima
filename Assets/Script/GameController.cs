afc71507697aaa78c2beb8515d95f9d466c12b3b
Maplewing <sinmaplewing@gmail.com>
2014-06-16 18:35:18 +0800

    OOAD2 Finished.

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-    public enum State { None, BattleStart, Running, MonsterAppear, 
-		DisplayQuestion, WaitingRequest, AnswerRight, AnswerWrong, PlayerDead };
-    private State _state = State.BattleStart;
+
+    public State State = new BattleStart();
+    public bool answerRight = false;
+
 
     public GameObject player;
 	public GameObject knight;
@@ -15,26 +16,26 @@ public class GameController : MonoBehaviour {
 	public GameObject planeGround;
 
     public GameObject GameOver;
-    private bool gameOverAnimationEnd = false;
+    public bool gameOverAnimationEnd = false;
     public GameObject StageClear;
-    private bool stageClearAnimationEnd = false;
+    public bool stageClearAnimationEnd = false;
 
     public GameObject enemyParticle;
-    private bool enemyParticleExec = false;
+    public bool enemyParticleExec = false;
     public GameObject playerParticle;
-    private bool playerParticleExec = false;
-    private float lifeValue;
+    public bool playerParticleExec = false;
+    public float lifeValue;
 
 
 	public GameObject lifeBar;
 
 	public GameObject Q, A, B, C, D;
 
-	private GameObject _nowMonster = null;
-	private int _timer = 0;
-	private bool _questionSelected = false;
-	private int _questionNumber = 0;
-	private GameObject _touchBg;
+	public GameObject nowMonster = null;
+    public int Timer = 0;
+	public bool questionSelected = false;
+	public int questionNumber = 0;
+	public GameObject touchBg;
 
     private Data _data;
 	// Use this for initialization
@@ -70,170 +71,17 @@ public class GameController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		PlayerDead ();
-		AnswerRight ();
-		AnswerWrong ();
-		WaitingRequest ();
-		DisplayQuestion ();
-		MonsterAppear ();
-		Running ();
-        BattleStart();
+        this.State.Execute(this);
 	}
 
-    void BattleStart()
-    {
-        if (_state == State.BattleStart)
-        {
-			if( player.transform.localPosition.x < -1.7f ){
-				player.transform.Translate(0.1f, 0, 0);
+	public bool GUIDisppear(){
+		if (touchBg.guiTexture.color.g < 0.7f && touchBg.guiTexture.color.r < 0.7f) {
+            if (answerRight)
+            {
+				touchBg.guiTexture.color += new Color(0, 0.1f, 0);
 			}
 			else{
-				_state = State.Running;
-				_timer = 0;
-			}
-        }
-    }
-
-	void Running(){
-		if (_state == State.Running) {
-            if (!stageClearAnimationEnd)
-            {
-                ++_timer;
-
-                if (Question.answerRightProblem >= Question.questions.Count)
-                {
-                    foreach (var question in Question.questions)
-                    {
-                        question.answerRight = false;
-                    }
-                    Question.answerRightProblem = 0;
-                    Instantiate(StageClear);
-                    stageClearAnimationEnd = true;
-                    PlayerPrefs.SetInt(Question.stageName, 1);
-                }
-
-                if (_timer >= 60)
-                {
-                    _state = State.MonsterAppear;
-                }
-            }
-            else
-            {
-                if (Input.touchCount > 0)
-                {
-                    Touch touch = Input.touches[0];
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        Application.LoadLevel("MainPage");
-                    }
-                }
-                if (Input.GetMouseButtonUp(0))
-                {
-                    Application.LoadLevel("MainPage");
-                }
-            }
-		}
-	}
-
-	void MonsterAppear(){
-		if( _state == State.MonsterAppear ){
-			if( _nowMonster == null ){
-				(_nowMonster = (Object.Instantiate(monster) as GameObject)).transform.position = new Vector3(4.45f, 1.40f, 17f);
-				_nowMonster.renderer.material.color = new Color( Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f) );
-				planeGround.GetComponent<Animator>().enabled = false;
-			}
-			else if(_nowMonster.transform.position.x > 1.1f){
-				_nowMonster.transform.position -= new Vector3(0.1f, 0, 0);
-			}
-			else {
-				_state = State.DisplayQuestion;
-				_questionSelected = false;
-			}
-		}
-	}
-
-	void DisplayQuestion(){
-		if (_state == State.DisplayQuestion) {
-			if( !_questionSelected ){
-                do {
-				    _questionNumber = Random.Range (0, Question.questions.Count*2);
-                    _questionNumber = _questionNumber % Question.questions.Count;
-                } while (Question.questions[_questionNumber].answerRight);
-                Q.GetComponentInChildren<GUIText>().text = Question.questions[_questionNumber].statement;
-				A.GetComponentInChildren<GUIText>().text = Question.questions[_questionNumber].a;
-				B.GetComponentInChildren<GUIText>().text = Question.questions[_questionNumber].b;
-				C.GetComponentInChildren<GUIText>().text = Question.questions[_questionNumber].c;
-				D.GetComponentInChildren<GUIText>().text = Question.questions[_questionNumber].d;
-				_questionSelected = true;
-			}
-
-			if( Q.guiTexture.color.a < 1 ){
-				Q.guiTexture.color += new Color( 0, 0, 0, 0.05f );
-			}
-			else if( A.transform.position.x < 0.35 ){
-				A.transform.Translate(0.1f, 0, 0, 0);
-                if (A.transform.position.x > 0.35)
-                {
-                    A.transform.position.Set(0.35f, A.transform.position.y, A.transform.position.z);
-                }
-			}
-			else if( B.transform.position.x > 0.66 ){
-                
-				B.transform.Translate(-0.1f, 0, 0, 0);
-                if (B.transform.position.x < 0.66)
-                {
-                    B.transform.position.Set(0.66f, B.transform.position.y, B.transform.position.z);
-                }
-			}
-			else if( C.transform.position.x < 0.35 ){
-				C.transform.Translate(0.1f, 0, 0, 0);
-                if (C.transform.position.x > 0.35)
-                {
-                    C.transform.position.Set(0.35f, C.transform.position.y, C.transform.position.z);
-                }
-			}
-			else if( D.transform.position.x > 0.66 ){
-				D.transform.Translate(-0.1f, 0, 0, 0);
-                if (D.transform.position.x < 0.66)
-                {
-                    D.transform.position.Set(0.66f, D.transform.position.y, D.transform.position.z);
-                }
-			}
-			else {
-				_state = State.WaitingRequest;
-			}
-		}
-
-	}
-
-	void WaitingRequest(){
-		if (_state == State.WaitingRequest) {
-			if ( Input.GetMouseButtonUp(0) || Input.touchCount > 0 ) {
-				GUILayer hit = Camera.main.GetComponent<GUILayer>();
-				GUIElement hitObject;
-				if ( Input.GetMouseButtonUp(0) ) hitObject = hit.HitTest(Input.mousePosition);
-				else hitObject = hit.HitTest(Input.touches[0].position);
-
-				if( hitObject != null && (hitObject.tag == "A" || hitObject.tag == "B" || hitObject.tag == "C" || hitObject.tag == "D") ){
-					_touchBg = GameObject.Find("battle_" + hitObject.tag);
-					if( hitObject.tag[0]-'A'+1 == Question.questions[_questionNumber].answer ){
-						_state = State.AnswerRight;
-					}
-					else{
-						_state = State.AnswerWrong;
-					}
-				}
-			}
-		}
-	}
-
-	bool GUIDisppear(){
-		if (_touchBg.guiTexture.color.g < 0.7f && _touchBg.guiTexture.color.r < 0.7f) {
-			if( _state == State.AnswerRight ){
-				_touchBg.guiTexture.color += new Color(0, 0.1f, 0);
-			}
-			else{
-				_touchBg.guiTexture.color += new Color(0.1f, 0, 0);
+				touchBg.guiTexture.color += new Color(0.1f, 0, 0);
 			}
 		}
 		else if( Q.guiTexture.color.a > 0 ){
@@ -252,94 +100,9 @@ public class GameController : MonoBehaviour {
 			D.transform.Translate(0.1f, 0, 0, 0);
 		}
 		else {
-			_touchBg.guiTexture.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+			touchBg.guiTexture.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
 			return true;
 		}
 		return false;
-	}
-
-	void AnswerWrong(){
-		if( _state == State.AnswerWrong ){
-			if( GUIDisppear()){
-                if (!playerParticleExec)
-                {
-                    playerParticle.particleEmitter.Emit();
-                    lifeValue = 0.4f;
-                    playerParticleExec = true;
-                }
-                if (lifeValue > 0)
-                {
-                    lifeBar.transform.Translate(-0.025f, 0, 0);
-                    lifeValue -= 0.025f;
-                }
-				else if( lifeBar.transform.position.x < -0.5f ){
-                    playerParticleExec = false;
-					_state = State.PlayerDead;
-				}
-				else{
-                    playerParticleExec = false;
-					_state = State.MonsterAppear;
-				}
-			}
-		}
-	}
-
-	void AnswerRight(){
-		if( _state == State.AnswerRight ){
-			if( GUIDisppear()){
-                if (!enemyParticleExec)
-                {
-                    enemyParticle.particleEmitter.Emit();
-                    enemyParticleExec = true;
-                }
-				if( _nowMonster.renderer.material.color.a > 0){
-					_nowMonster.renderer.material.color -= new Color(0, 0, 0, 0.1f);
-				}
-				else{
-					_timer = 0;
-                    enemyParticleExec = false;
-					Destroy( _nowMonster );
-                    Question.questions[_questionNumber].answerRight = true;
-                    Question.answerRightProblem++;
-					//Character.score += 1;
-					planeGround.GetComponent<Animator>().enabled = true;
-					_state = State.Running;
-				}
-			}
-		}
-	}
-
-	void PlayerDead(){
-		if (_state == State.PlayerDead) {
-			SpriteRenderer sprite = player.GetComponentInChildren<SpriteRenderer>();
-			if( sprite.color.a > 0 ){
-				sprite.color -= new Color(0, 0, 0, 0.1f);
-			}
-            else if (!gameOverAnimationEnd)
-            {
-                foreach (var question in Question.questions)
-                {
-                    question.answerRight = false;
-                }
-                Question.answerRightProblem = 0;
-                Instantiate(GameOver);
-                gameOverAnimationEnd = true;
-            }
-            else
-            {
-                if (Input.touchCount > 0)
-                {
-                    Touch touch = Input.touches[0];
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        Application.LoadLevel("MainPage");
-                    }
-                }
-                if (Input.GetMouseButtonUp(0))
-                {
-                    Application.LoadLevel("MainPage");
-                }
-            }
-		}
 	}
 }
---
+++ Assets/Script/State.cs
@@ -0,0 +1,286 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class State : MonoBehaviour
+{
+    public State() { }
+    public abstract void Execute(GameController controller);
+}
+
+public class BattleStart : State
+{
+    public override void Execute(GameController controller)
+    {
+        if (controller.player.transform.localPosition.x < -1.7f)
+        {
+            controller.player.transform.Translate(0.1f, 0, 0);
+        }
+        else
+        {
+            controller.State = new Running();
+            controller.Timer = 0;
+        }
+    }
+}
+
+public class Running : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        if (!controller.stageClearAnimationEnd)
+        {
+            ++controller.Timer;
+
+            if (Question.answerRightProblem >= Question.questions.Count)
+            {
+                foreach (var question in Question.questions)
+                {
+                    question.answerRight = false;
+                }
+                Question.answerRightProblem = 0;
+                Instantiate(controller.StageClear);
+                controller.stageClearAnimationEnd = true;
+                PlayerPrefs.SetInt(Question.stageName, 1);
+            }
+
+            if (controller.Timer >= 60)
+            {
+                controller.State = new MonsterAppear();
+            }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.touches[0];
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Application.LoadLevel("MainPage");
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                Application.LoadLevel("MainPage");
+            }
+        }
+    }
+}
+
+public class MonsterAppear : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        if (controller.nowMonster == null)
+        {
+            (controller.nowMonster = (Object.Instantiate(controller.monster) as GameObject)).transform.position = new Vector3(4.45f, 1.40f, 17f);
+            controller.nowMonster.renderer.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            controller.planeGround.GetComponent<Animator>().enabled = false;
+        }
+        else if (controller.nowMonster.transform.position.x > 1.1f)
+        {
+            controller.nowMonster.transform.position -= new Vector3(0.1f, 0, 0);
+        }
+        else
+        {
+            controller.State = new DisplayQuestion();
+            controller.questionSelected = false;
+        }
+    }
+}
+
+public class DisplayQuestion : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        if (!controller.questionSelected)
+        {
+            do
+            {
+                controller.questionNumber = Random.Range(0, Question.questions.Count * 2);
+                controller.questionNumber = controller.questionNumber % Question.questions.Count;
+            } while (Question.questions[controller.questionNumber].answerRight);
+            controller.Q.GetComponentInChildren<GUIText>().text = Question.questions[controller.questionNumber].statement;
+            controller.A.GetComponentInChildren<GUIText>().text = Question.questions[controller.questionNumber].a;
+            controller.B.GetComponentInChildren<GUIText>().text = Question.questions[controller.questionNumber].b;
+            controller.C.GetComponentInChildren<GUIText>().text = Question.questions[controller.questionNumber].c;
+            controller.D.GetComponentInChildren<GUIText>().text = Question.questions[controller.questionNumber].d;
+            controller.questionSelected = true;
+        }
+
+        if (controller.Q.guiTexture.color.a < 1)
+        {
+            controller.Q.guiTexture.color += new Color(0, 0, 0, 0.05f);
+        }
+        else if (controller.A.transform.position.x < 0.35)
+        {
+            controller.A.transform.Translate(0.1f, 0, 0, 0);
+            if (controller.A.transform.position.x > 0.35)
+            {
+                controller.A.transform.position.Set(0.35f, controller.A.transform.position.y, controller.A.transform.position.z);
+            }
+        }
+        else if (controller.B.transform.position.x > 0.66)
+        {
+
+            controller.B.transform.Translate(-0.1f, 0, 0, 0);
+            if (controller.B.transform.position.x < 0.66)
+            {
+                controller.B.transform.position.Set(0.66f, controller.B.transform.position.y, controller.B.transform.position.z);
+            }
+        }
+        else if (controller.C.transform.position.x < 0.35)
+        {
+            controller.C.transform.Translate(0.1f, 0, 0, 0);
+            if (controller.C.transform.position.x > 0.35)
+            {
+                controller.C.transform.position.Set(0.35f, controller.C.transform.position.y, controller.C.transform.position.z);
+            }
+        }
+        else if (controller.D.transform.position.x > 0.66)
+        {
+            controller.D.transform.Translate(-0.1f, 0, 0, 0);
+            if (controller.D.transform.position.x < 0.66)
+            {
+                controller.D.transform.position.Set(0.66f, controller.D.transform.position.y, controller.D.transform.position.z);
+            }
+        }
+        else
+        {
+            controller.State = new WaitingRequest();
+        }
+    }
+}
+
+public class WaitingRequest : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        if (Input.GetMouseButtonUp(0) || Input.touchCount > 0)
+        {
+            GUILayer hit = Camera.main.GetComponent<GUILayer>();
+            GUIElement hitObject;
+            if (Input.GetMouseButtonUp(0)) hitObject = hit.HitTest(Input.mousePosition);
+            else hitObject = hit.HitTest(Input.touches[0].position);
+
+            if (hitObject != null && (hitObject.tag == "A" || hitObject.tag == "B" || hitObject.tag == "C" || hitObject.tag == "D"))
+            {
+                controller.touchBg = GameObject.Find("battle_" + hitObject.tag);
+                if (hitObject.tag[0] - 'A' + 1 == Question.questions[controller.questionNumber].answer)
+                {
+                    controller.answerRight = true;
+                    controller.State = new AnswerRight();
+                }
+                else
+                {
+                    controller.answerRight = false;
+                    controller.State = new AnswerWrong();
+                }
+            }
+        }
+    }
+}
+
+public class AnswerRight : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        if (controller.GUIDisppear())
+        {
+            if (!controller.enemyParticleExec)
+            {
+                controller.enemyParticle.particleEmitter.Emit();
+                controller.enemyParticleExec = true;
+            }
+            if (controller.nowMonster.renderer.material.color.a > 0)
+            {
+                controller.nowMonster.renderer.material.color -= new Color(0, 0, 0, 0.1f);
+            }
+            else
+            {
+                controller.Timer = 0;
+                controller.enemyParticleExec = false;
+                Destroy(controller.nowMonster);
+                Question.questions[controller.questionNumber].answerRight = true;
+                Question.answerRightProblem++;
+                //Character.score += 1;
+                controller.planeGround.GetComponent<Animator>().enabled = true;
+                controller.State = new Running();
+            }
+        }
+    }
+}
+
+public class AnswerWrong : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        if (controller.GUIDisppear())
+        {
+            if (!controller.playerParticleExec)
+            {
+                controller.playerParticle.particleEmitter.Emit();
+                controller.lifeValue = 0.4f;
+                controller.playerParticleExec = true;
+            }
+            if (controller.lifeValue > 0)
+            {
+                controller.lifeBar.transform.Translate(-0.025f, 0, 0);
+                controller.lifeValue -= 0.025f;
+            }
+            else if (controller.lifeBar.transform.position.x < -0.5f)
+            {
+                controller.playerParticleExec = false;
+                controller.State = new PlayerDead();
+            }
+            else
+            {
+                controller.playerParticleExec = false;
+                controller.State = new MonsterAppear();
+            }
+        }
+    }
+}
+
+public class PlayerDead : State
+{
+
+    public override void Execute(GameController controller)
+    {
+        SpriteRenderer sprite = controller.player.GetComponentInChildren<SpriteRenderer>();
+        if (sprite.color.a > 0)
+        {
+            sprite.color -= new Color(0, 0, 0, 0.1f);
+        }
+        else if (!controller.gameOverAnimationEnd)
+        {
+            foreach (var question in Question.questions)
+            {
+                question.answerRight = false;
+            }
+            Question.answerRightProblem = 0;
+            Instantiate(controller.GameOver);
+            controller.gameOverAnimationEnd = true;
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.touches[0];
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Application.LoadLevel("MainPage");
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                Application.LoadLevel("MainPage");
+            }
+        }
+    }
+}
